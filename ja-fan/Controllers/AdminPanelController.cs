b6a6729f8dc3404e146e3e69574ab1a4e8ed03d2
s-tyda <styda@huhha.co>
2022-06-19 20:46:26 +0200

    Added admin panel

---
+++ ja-fan/Controllers/AdminPanelController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using ja_fan.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ja_fan.Controllers;
+
+public class AdminPanelController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    
+    public AdminPanelController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [Authorize]
+    public IActionResult Index()
+    {
+        return View();
+    }
+    
+    [Authorize]
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
